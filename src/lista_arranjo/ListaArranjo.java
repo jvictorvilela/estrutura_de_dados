3a69dc4cfd62adff3f892831d94447ccb0ec82dd
João Victor <victorvilela00@gmail.com>
2016-10-04 00:21:49 -0300

    Mudando nome de arquivo Lista para ListaArranjo e excluindo arquivo desnecessáro.

--- src/lista_arranjo/ListaArranjo.java
+++ src/lista_arranjo/ListaArranjo.java
@@ -5,7 +5,67 @@ package lista_arranjo;
  * @author João Victor Vilela
  */
 public class ListaArranjo {
+    private int tam; // Tamanho atual do array.
+    private int ultimo; // Indica a posição do ultimo elemento + 1.
+    private int i;
+    private Object lista[];
+    private Object listaAuxiliar[];
+    
     public ListaArranjo() {
-        Lista novaLista = new Lista();
+        this.ultimo = 0;
+        this.tam = 10;
+        this.lista = new Object[tam];
     }
+    
+    public void ampliarLista() {
+        this.listaAuxiliar = new Object[tam*2];
+        
+        // Copiando os itens do array antigo para o array auxiliar.
+        i = 0;
+        for (Object aux : lista) {
+            this.listaAuxiliar[i] = aux;
+            this.i++;
+        }
+        this.lista = this.listaAuxiliar;
+        this.tam = this.tam*2;
+    }
+    
+    // Método que recebe um índice e um item, e adiciona o item no índice informado.
+    public boolean adicionar(int indice, Object item) {
+        // Verifica possíveis erros.
+        if (indice < 0 || indice > this.tam) {
+            return false;
+        }
+        
+        // Verifica se a lista está cheia.
+        if (this.ultimo > this.tam) {
+            this.ampliarLista();
+        }
+        
+        if (indice == this.ultimo) {
+            this.lista[indice] = item;
+        } else {
+            // Realoca os itens da lista
+            for (i = this.ultimo-1; i >= indice; i--) {
+                this.lista[i+1] = this.lista[i];
+            }
+            this.lista[indice] = item;
+        }   
+        
+        ultimo++;
+        return true;
+    }
+    
+    // Método que adiciona item no final da lista.
+    public boolean adicionarFinal(Object item) {
+        return this.adicionar(this.ultimo, item);
+    }
+    
+    // Método que imprime os itens da lista.
+    public void imprimirLista() {
+        for (Object aux : lista) {
+            System.out.println(aux);
+        }
+    }
+    
 }
