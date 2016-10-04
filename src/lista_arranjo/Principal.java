11d530166f4b69426d512ecf248e2f4db1d4267a
João Victor <victorvilela00@gmail.com>
2016-10-03 23:53:39 -0300

    Classe Lista criada, e método ampliarLista() criado.

--- src/lista_arranjo/Principal.java
+++ src/lista_arranjo/Principal.java
@@ -1,13 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package lista_arranjo;
 
 /**
  *
- * @author victor
+ * @author João Victor Vilela
  */
 public class Principal {
     public static void main(String[] args) {
